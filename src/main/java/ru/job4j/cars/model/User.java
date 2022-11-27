e1abd670194b09e5b97656611e20808f03e017c1
MasterPC <free_person@inbox.ru>
2022-11-27 19:07:27 +0300

    6. Лямбды и шаблон commander [#49295]

--- src/main/java/ru/job4j/cars/model/User.java
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -1,4 +1,4 @@
-package ru.job4j.model;
+package ru.job4j.cars.model;
 
 import lombok.Data;
 
---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,85 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import ru.job4j.cars.model.User;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final CrudRepository crudRepository;
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        crudRepository.run(session -> session.save(user));
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        crudRepository.run(
+                "UPDATE User SET login = :fLogin, password = :fPassword WHERE id = :fId",
+                Map.of("fLogin", user.getLogin(), "fPassword", user.getPassword(), "fId", user.getId())
+        );
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        crudRepository.run("DELETE User WHERE id = :fId", Map.of("fId", userId));
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        return crudRepository.query("from User", User.class);
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @param userId ID
+     * @return Optional of user.
+     */
+    public Optional<User> findById(int userId) {
+        return crudRepository.optional(
+                "from User where id = :fId", User.class,
+                Map.of("fId", userId)
+        );
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        return crudRepository.query(
+                "from User where login LIKE :fKey", User.class,
+                Map.of("fKey", "%" + key + "%")
+        );
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional of user.
+     */
+    public Optional<User> findByLogin(String login) {
+        return crudRepository.optional(
+                "from User where login = :fLogin", User.class,
+                Map.of("fLogin", login)
+        );
+    }
+}
