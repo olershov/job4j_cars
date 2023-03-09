16bfc6c7fb9f1a2157a632084f36a208f922ff9c
MasterPC <free_person@inbox.ru>
2023-03-09 21:29:52 +0300

    Контроллер отображения всех объявлений.

--- src/main/java/ru/job4j/cars/Main.java
+++ src/main/java/ru/job4j/cars/Main.java
@@ -23,6 +23,6 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        System.out.println("Go to http://localhost:8080/posts/");
+        System.out.println("Go to http://localhost:8080/post/");
     }
 }
--- src/main/java/ru/job4j/cars/controller/PostController.java
+++ src/main/java/ru/job4j/cars/controller/PostController.java
@@ -2,12 +2,52 @@ package ru.job4j.cars.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.cars.model.Post;
 import ru.job4j.cars.service.post.PostService;
+import java.io.IOException;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 
+@RequestMapping("/post")
 @AllArgsConstructor
 @Controller
 public class PostController {
 
     private final PostService postService;
 
+    @GetMapping("/")
+    public String all(Model model) {
+        model.addAttribute("posts", postService.findAllOrderById());
+        return "post/all";
+    }
+
+    @GetMapping("/formAddPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post());
+        return "post/add";
+    }
+
+    @PostMapping("/createPost")
+    public String createSession(@ModelAttribute Post post,
+                                @RequestParam("file") MultipartFile file) throws IOException {
+        post.setPhoto(file.getBytes());
+        postService.create(post);
+        return "redirect:/post/";
+    }
+
+    @GetMapping("/photoPost/{postId}")
+    public ResponseEntity<Resource> download(@PathVariable("postId") Integer postId) {
+        Post post = postService.findById(postId).get();
+        return ResponseEntity.ok()
+                .headers(new HttpHeaders())
+                .contentLength(post.getPhoto().length)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new ByteArrayResource(post.getPhoto()));
+    }
 }
